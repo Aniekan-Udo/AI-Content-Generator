22cafe8d6c6aa31a96f820f849782a7ff869b01f
Aniekan Udo <aniekanudo476@gmail.com>
2025-10-06 07:39:08 +0100

    update .env

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ os.environ["USER_AGENT"] = os.getenv("USER_AGENT", "ai-content-generator/0.1")
 os.environ['STREAMLIT_SERVER_FILE_WATCHER_TYPE'] = 'none'
 
 # Hardcoded API key
-GROQ_API_KEY = os.getenv("API_KEY")
+GROQ_API_KEY = "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"
 if not GROQ_API_KEY:
     raise ValueError("API_KEY environment variable not set.")
 
